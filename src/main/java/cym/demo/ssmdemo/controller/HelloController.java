eee7bc0491c5570faf480f7063e01cc8acda9684
chenyouming <chenyouming296@163.com>
2019-11-26 14:43:19 +0800

    SpringMVC请求处理

--- src/main/java/cym/demo/ssmdemo/controller/HelloController.java
+++ src/main/java/cym/demo/ssmdemo/controller/HelloController.java
@@ -1,11 +1,11 @@
 package cym.demo.ssmdemo.controller;
 
+import cym.demo.ssmdemo.bean.TbUser;
 import cym.demo.ssmdemo.bean.TbUserExample;
 import cym.demo.ssmdemo.dao.TbUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Administrator on 2019/11/22.
@@ -15,10 +15,52 @@ public class HelloController {
 
     @Autowired
     TbUserMapper userMapper;
+
+    /**
+     * 使用注解@RequestParam获取参数
+     * @param input 接收请求参数
+     * @return  页面名称
+     */
     @RequestMapping(value = "/hello")
-    public String hello(@RequestParam String input) {
-        System.out.println(userMapper.countByExample(new TbUserExample()));
+    public String hello(@RequestParam("dd") String input) {
+//        System.out.println(userMapper.countByExample(new TbUserExample()));
         System.out.println(input);
         return "second";
+    }
+
+    /**
+     * 一般请求
+     * @return 跳转three.html
+     */
+    @RequestMapping(value = "/thre")
+    public String three() {
+        return "three";
+    }
+
+    /**
+     * 接收url请求的封装对象，如form表单，
+     * @param tbUser 实体类
+     * @return three.html
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/user")
+    public String user(TbUser tbUser) {
+//        System.out.println(uname);
+//        System.out.println(upass);
+//        System.out.println(userId);
+        System.out.println(tbUser);
+        return "three";
+    }
+
+    /**
+     * RequestBody接收application/json请求内容的json串
+     * @param tbUser 实体类
+     * @return json对象
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/user2")
+    @ResponseBody
+    public Object user2(@RequestBody TbUser tbUser) {
+
+        System.out.println(tbUser);
+        return tbUser;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,17 @@
 <html>
+<head>
+    <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+</head>
 <body>
 <h2>Hello World!</h2>
 <form method="get" action="/hello">
-    <input type="text" name="input"/>
+    <input type="text" name="dd"/>
     <input type="submit" value="ClickMe"/>
 </form>
+<script type="text/javascript">
+    $(function () {
+        console.log(22);
+    })
+</script>
 </body>
 </html>
